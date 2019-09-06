e5ad3b9d881d0cb56ce88a91fbc552190a886519
TJPUZhangHui <1059859967@qq.com>
2019-09-06 08:31:23 -0400

    led driver

--- 01_rk3399-gpio/.rk_led_drv.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/book/nfs_rootfs/01_rk3399-gpio/rk_led_drv.ko := aarch64-linux-gnu-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/book/nfs_rootfs/01_rk3399-gpio/rk_led_drv.ko /home/book/nfs_rootfs/01_rk3399-gpio/rk_led_drv.o /home/book/nfs_rootfs/01_rk3399-gpio/rk_led_drv.mod.o
--- 01_rk3399-gpio/.tmp_versions/rk_led_drv.mod
+++
@@ -1,2 +0,0 @@
-/home/book/nfs_rootfs/01_rk3399-gpio/rk_led_drv.ko
-/home/book/nfs_rootfs/01_rk3399-gpio/rk_led_drv.o
