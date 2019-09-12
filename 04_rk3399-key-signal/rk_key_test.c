52aa2a066cd50f27e1289d6ee8abcc40fababe85
TJPUZhangHui <1059859967@qq.com>
2019-09-12 22:08:01 +0800

    signal

---
+++ 04_rk3399-key-signal/rk_key_test.c
@@ -0,0 +1,47 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <poll.h>
+#include <signal.h>
+#include <sys/types.h>
+
+static int fd;
+
+void my_handle(int num)
+{
+	int val;
+	read(fd, &val, sizeof(val)); 
+	printf("val = %d\n", val);
+}
+
+int main(int argc, char **argv)
+{
+	int res;
+	int flag;
+	
+	fd = open("/dev/key", O_RDWR);
+	if (fd < 0)
+	{
+		printf("can't open!\n");
+	}
+
+	/* 设置设备文件的拥有者为本进程 */
+	fcntl(fd, F_SETOWN, getpid()); 
+	flag = fcntl(fd, F_GETFL); 
+	fcntl(fd, F_SETFL, flag | FASYNC);
+	
+	signal(SIGIO, my_handle);
+	
+	while (1)
+	{
+		sleep(1000);
+	}
+
+	return 0;
+}
+
+
+
--- README.md
+++ README.md
@@ -1,12 +1,22 @@
-## ROC-RK3399-PC  ##
+## board : ROC-RK3399-PC  ##
 
-2019-09-06 add led driver v1.0                   
+### 1 led 驱动 ###
 
-2019-09-10 add key driver v1.0  
+**2019-09-06 add led driver v1.0   **                
 
-2019-09-12 add key driver v1.1
-	poll 
+### 2 按键驱动 ###
 
-author : zhang hui 
+**2019-09-10 add key driver v1.0 ** 
+- 阻塞IO模型，等待队列
+
+**2019-09-12 add key driver v1.1**
+- 多路IO复用模型，poll机制
+
+**2019-09-12 add key driver v1.2**
+- 异步通知IO模型
+
+
+### author : hui zhang ###
+### hexo : tjpuzhanghui.github.io ###
 
 
